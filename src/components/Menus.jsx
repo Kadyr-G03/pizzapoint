188d59b0b3f16c16d7fdb13f2d196ac8e6b0e65f
Kadyr <kadyr.g03@gmail.com>
2024-10-30 21:20:27 +0200

    commited

---
+++ src/components/Menus.jsx
@@ -0,0 +1,26 @@
+function Menus() {
+
+    return (
+      <>
+          <li className="mx-3">
+              <a onClick={() => scrollToSection('gallery')} href="">Lorem</a>
+          </li>
+          <li className="mx-3">
+              <a onClick={() => scrollToSection('gallery')} href="">Lorem</a>
+          </li>
+          <li className="mx-3">
+              <a onClick={() => scrollToSection('gallery')} href="">Lorem</a>
+          </li>
+          <li className="mx-3">
+              <a onClick={() => scrollToSection('gallery')} href="">Lorem</a>
+          </li>
+      </>
+    )
+  }
+  
+  export default Menus
+  
+  
+  
+  
+  
